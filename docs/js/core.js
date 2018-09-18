bcdc38d882818619e27e1b710b5f2c39fc934eea
oldbinky <43254066+oldbinky@users.noreply.github.com>
2018-09-18 17:15:12 +0100

    Update core.js

--- docs/js/core.js
+++ docs/js/core.js
@@ -27,7 +27,8 @@ emt = [
     { id: "ste", src: "img/se.png", name: "ST Elevation", current: false },
     { id: "vf", src: "img/vf.png",  name: "Ventricular Fibrillation", current: false },
     { id: "vt", src: "img/vt.png",  name: "Ventricular Tachycardia", current: false },
-    { id: "vt", src: "img/tt.png",  name: "test", current: false },
+    { id: "tt", src: "img/tt.png",  name: "test", current: false },
+    { id: "SRV", src: "mp4/SRV.gif.png",  name: "Sinus vid", current: false },
     // { id: "xxvid", src: "mp4/sinus-rhythm.mp4", name: "Sinus Rhythm (Video)", current: false, video: true }
 ]
 
